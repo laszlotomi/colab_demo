1a7171137db184b7ebb465a2c1e2143a83c2b61b
Tamas Laszlo <tamas.laszlo@t-systems.com>
2020-02-28 13:22:14 +0100

    test2

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -34,13 +34,7 @@ FLAGS = flags.FLAGS
 def class_text_to_int(row_label):
     if row_label == 'router':
         return 1
-"""    
-elif row_label == 'Router2':
-        return 2
 
-    else:
-        None
-"""
 
 def split(df, group):
     data = namedtuple('data', ['filename', 'object'])
