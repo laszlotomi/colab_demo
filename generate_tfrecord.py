13ac605d42cb23090879e103df486068537a8bf0
Tamas Laszlo <tamas.laszlo@t-systems.com>
2020-02-28 13:05:03 +0100

    update 1 class

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -34,8 +34,10 @@ FLAGS = flags.FLAGS
 def class_text_to_int(row_label):
     if row_label == 'router':
         return 1
-    elif row_label == 'Router2':
+"""    
+elif row_label == 'Router2':
         return 2
+"""
     else:
         None
 
